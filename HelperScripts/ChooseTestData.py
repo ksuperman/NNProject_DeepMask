a1b0f4c45b5072f880eb17bec352dbe6b8eb714e
Ryan Baumann <ryan.baumann@gmail.com>
2016-08-09 13:18:54 -0400

    ChooseTestData for both train and test

--- HelperScripts/ChooseTestData.py
+++ HelperScripts/ChooseTestData.py
@@ -2,13 +2,18 @@ import numpy as np
 import glob
 import shutil
 
-source_dir = '../Results/neg-train'
-dest_dir = '../Predictions/train'
+dirs = [['../Results/neg-train','../Predictions/train'],['../Results/pos-train','../Predictions/train'],['../Results/neg-val','../Predictions/test'],['../Results/pos-val','../Predictions/test']]
 
-ex_paths = glob.glob('%s/*-im.png' % source_dir)
-np.random.shuffle(ex_paths)
-selected = ex_paths[0:8000]
+for pair in dirs:
+    source_dir = pair[0]
+    dest_dir = pair[1]
+    print source_dir
+    print dest_dir
 
-for sel in selected:
-    shutil.copy(sel, dest_dir)
-    shutil.copy(sel.replace('im', 'mask'), dest_dir)
+    ex_paths = glob.glob('%s/*-im.png' % source_dir)
+    np.random.shuffle(ex_paths)
+    selected = ex_paths[0:8000]
+
+    for sel in selected:
+        shutil.copy(sel, dest_dir)
+        shutil.copy(sel.replace('im', 'mask'), dest_dir)
